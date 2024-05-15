5e9cfb83a8b1315c8b7381ad7948608444f7c5f6
Luisfn06 <161611949+Luisfn06@users.noreply.github.com>
2024-05-15 10:53:44 -0300

    Adicionei função de mudar Tema

--- Codigo/script.js
+++ Codigo/script.js
@@ -1,5 +1,20 @@
-var header = document.getElementById('header');
+var body = document.getElementById('body');
+var mudarTemaEscuro = document.getElementById('mudarTemaEscuro');
+var mudarTemaClaro = document.getElementById('mudarTemaClaro');
 
-header.addEventListener('abort', function(){
-
+mudarTemaClaro.addEventListener('load', function(){
+    mudarTemaClaro.style.display = 'none';
 })
+mudarTemaEscuro.addEventListener('click', function(){
+mudarTemaEscuro.style.display = 'none'
+body.style.backgroundColor = 'black';
+mudarTemaClaro.style.display = 'inline';
+
+});
+mudarTemaClaro.addEventListener('click', function(){
+    mudarTemaClaro.style.display = 'none'
+    body.style.backgroundColor = 'white';
+    mudarTemaEscuro.style.display = 'inline';
+    
+    });
+
