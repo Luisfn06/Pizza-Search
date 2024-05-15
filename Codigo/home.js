8d0a8d6147c53b2e134d2410ccfe476ade01d528
Luisfn06 <161611949+Luisfn06@users.noreply.github.com>
2024-05-15 20:04:45 -0300

    Trasicao ao mudar tema

--- Codigo/home.js
+++ Codigo/home.js
@@ -1,6 +1,7 @@
 var body = document.getElementById('body');
 var mudarTemaEscuro = document.getElementById('mudarTemaEscuro');
 var mudarTemaClaro = document.getElementById('mudarTemaClaro');
+var textoLiberdade = document.getElementById('textoLiberdade');
 
 mudarTemaClaro.addEventListener('load', function(){
     mudarTemaClaro.style.display = 'none';
@@ -8,14 +9,18 @@ mudarTemaClaro.addEventListener('load', function(){
 mudarTemaEscuro.addEventListener('click', function(){
 mudarTemaEscuro.style.display = 'none'
 body.style.backgroundColor = 'black';
+body.style.transition = ' 0.50s ease';
 mudarTemaClaro.style.display = 'inline';
+textoLiberdade.style.color = 'white'
+
 
 });
 mudarTemaClaro.addEventListener('click', function(){
     mudarTemaClaro.style.display = 'none'
     body.style.backgroundColor = 'white';
     mudarTemaEscuro.style.display = 'inline';
-    
+    textoLiberdade.style.color = 'black'
+
     });
 
 
