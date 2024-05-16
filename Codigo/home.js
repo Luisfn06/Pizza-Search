ff7d481a82477443ddb752d568390824507aaa7e
Luisfn06 <161611949+Luisfn06@users.noreply.github.com>
2024-05-16 11:54:52 -0300

    Atualizacoes

--- Codigo/home.js
+++ Codigo/home.js
@@ -1,26 +1,44 @@
 var body = document.getElementById('body');
-var mudarTemaEscuro = document.getElementById('mudarTemaEscuro');
-var mudarTemaClaro = document.getElementById('mudarTemaClaro');
+var mudarTemaEscuro = document.querySelectorAll('.mudarTemaEscuro');
+var mudarTemaClaro = document.querySelectorAll('.mudarTemaClaro');
 var textoLiberdade = document.getElementById('textoLiberdade');
+var header = document.querySelector('header');
+var botaoNavbar = document.getElementById('botaoNavbar');
 
-mudarTemaClaro.addEventListener('load', function(){
-    mudarTemaClaro.style.display = 'none';
-})
-mudarTemaEscuro.addEventListener('click', function(){
-mudarTemaEscuro.style.display = 'none'
-body.style.backgroundColor = 'black';
-body.style.transition = ' 0.50s ease';
-mudarTemaClaro.style.display = 'inline';
-textoLiberdade.style.color = 'white'
+// Adicione o event listener a cada ícone do sol para o clique
+mudarTemaEscuro.forEach(function(icon) {
+    icon.addEventListener('click', function() {
+        // Oculta todos os ícones do sol
+        mudarTemaEscuro.forEach(function(icon) {
+            icon.style.display = 'none';
+        });
 
-
-});
-mudarTemaClaro.addEventListener('click', function(){
-    mudarTemaClaro.style.display = 'none'
-    body.style.backgroundColor = 'white';
-    mudarTemaEscuro.style.display = 'inline';
-    textoLiberdade.style.color = 'black'
-
+        // Muda o tema para escuro
+        body.style.backgroundColor = 'black';
+        header.style.backgroundColor = 'black';
+        mudarTemaClaro.forEach(function(icon) {
+            icon.style.display = 'inline';
+        });
+        textoLiberdade.style.color = 'white';
+        body.style.transition = '0.50s ease';
+        botaoNavbar.style.backgroundColor = 'white'
     });
+});
 
+// Adicione o event listener a cada ícone da lua para o clique
+mudarTemaClaro.forEach(function(icon) {
+    icon.addEventListener('click', function() {
+        // Oculta todos os ícones da lua
+        mudarTemaClaro.forEach(function(icon) {
+            icon.style.display = 'none';
+        });
 
+        // Muda o tema para claro
+        body.style.backgroundColor = 'white';
+        header.style.backgroundColor = 'rgb(139, 21, 21)';
+        mudarTemaEscuro.forEach(function(icon) {
+            icon.style.display = 'inline';
+        });
+        textoLiberdade.style.color = 'black';
+    });
+});
